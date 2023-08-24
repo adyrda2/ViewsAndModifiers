164519187b2368b878efa7c4a59da2f57b52ec73
Angela Dyrda <adyrda2@gmail.com>
2023-08-23 19:26:09 -0500

    Day 24 challenge: Add custom modifier

---
+++ ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Angela Dyrda on 8/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+                .prominentTitleStyle()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+// Custom modifier
+struct ProminentTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
+    }
+}
+
+extension View {
+    func prominentTitleStyle() -> some View {
+        modifier(ProminentTitle())
+    }
+}
